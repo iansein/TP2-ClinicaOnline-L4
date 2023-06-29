aabe5e1764b0e394103d3ad155627553cb9e9d26
Ian Sein <78178685+iansein@users.noreply.github.com>
2023-06-29 04:36:47 -0300

    Delete Tp-ClinicaOnline-IanSein/app directory

--- Tp-ClinicaOnline-IanSein/app/directivas/boton.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BotonDirective } from './boton.directive';
-
-describe('BotonDirective', () => {
-  it('should create an instance', () => {
-    const directive = new BotonDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- Tp-ClinicaOnline-IanSein/app/pipes/day-date.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DayDatePipe } from './day-date.pipe';
-
-describe('DayDatePipe', () => {
-  it('create an instance', () => {
-    const pipe = new DayDatePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
